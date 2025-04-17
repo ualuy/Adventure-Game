2e933421bd005204350fb6e1e7897b05577fdbf4
ualuy <chesterachit@gmail.com>
2025-04-17 14:45:19 -0500

    added prompts in script

--- game.sh
+++ game.sh
@@ -1,1 +1,12 @@
 echo "Welcome to the Adventure Game!"
+echo "Choose the location to explore: forest, castle, or cave."
+read location
+if ["$location" == "forest"]; then 
+	cat forest.txt
+elif ["$location" == "castle"]; then
+	cat castle.txt
+elif ["$location" == "cave"]; then
+	cat cave.txt
+else
+	echo "Please reenter location as location was invalid."
+fi
