9a52d55bac2290accfdfe8b017bb7ccceef1361b
ualuy <chesterachit@gmail.com>
2025-04-17 15:27:23 -0500

    added game2.0

--- game.sh
+++ game.sh
@@ -1,11 +1,12 @@
-echo "Welcome to the Adventure Game!"
+#!/bin/bash
+echo "Welcome to the Ultimate Adventure Game!"
 echo "Choose the location to explore: forest, castle, or cave."
 read location
-if ["$location" == "forest"]; then 
+if [ "$location" == "forest" ]; then 
 	cat forest.txt
-elif ["$location" == "castle"]; then
+elif [ "$location" == "castle" ]; then
 	cat castle.txt
-elif ["$location" == "cave"]; then
+elif [ "$location" == "cave" ]; then
 	cat cave.txt
 	echo "but then!"
 	cat mon.txt
