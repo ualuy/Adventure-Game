7ed51cee0760d56d7a614313dafe3006f1d7bdb0
ualuy <chesterachit@gmail.com>
2025-04-22 15:30:51 -0500

    added game3.0

--- game.sh
+++ game.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 echo "Welcome to the Ultimate Adventure Game!"
+echo "Make your own character! What is your character's name?"
+read name
+echo "$name... That's a great name! What is your character's role?"
+echo "mage       tank       assasin"
+read role
+if [ "$role" == "mage" ]; then
+	cat mage.txt
+	cat mweak.txt
+elif [ "$role" == "tank" ]; then
+	cat tank.txt
+	cat tweak.txt
+elif [ "$role" == "assasin" ]; then
+	cat assasin.txt
+	cat aweak.txt
+else
+	echo "Please reenter role as role was invalid."
+fi
+
 echo "Choose the location to explore: forest, castle, or cave."
+
 read location
-if [ "$location" == "forest" ]; then 
+if [ "$location" == "forest" ]; then
 	cat forest.txt
 elif [ "$location" == "castle" ]; then
 	cat castle.txt
@@ -14,3 +33,4 @@ elif [ "$location" == "cave" ]; then
 else
 	echo "Please reenter location as location was invalid."
 fi
+
